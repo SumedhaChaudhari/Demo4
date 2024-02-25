a95a6900d8cbbf50a4a8f3746cae67fe21adb5c4
SumedhaChaudhari <sumedhabsl@gmail.com>
2024-02-25 16:37:17 +0530

    react code

--- mess/src/App.js
+++ mess/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Subscribecomp from './Components/Subscribecomp';
+import Menuupdatecomp from './Components/Menuupdatecomp';
+import vendorsignupcomp, { Vendorsignupcomp } from './Components/vendorsignupcomp';
+import VendorsComp from './Components/VendorsComp';
+import IndividualVendorComp from './Components/IndividualVendorComp';
+import CustomerDeliveryComp from './Components/CustomerDeliveryComp';
+import RouterComp from './Components/RouterComp';
+import RoutingComp from './Components/RoutingComp';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Subscribecomp></Subscribecomp> */}
+      {/* <Menuupdatecomp></Menuupdatecomp> */}
+      {/* <Vendorsignupcomp></Vendorsignupcomp> */}
+
+    
+      {/* <VendorsComp/> */}
+      {/* <IndividualVendorComp/> */}
+
+      {/* <CustomerDeliveryComp/> */}
+
+      <RoutingComp/>
     </div>
   );
 }
